2b82c7fe8fcefc6962e9010056fc1d87026cb5bc
Hoda Zare <zare.hoda@gmail.com>
2020-03-30 13:30:39 -0400

    next largest permutation

---
+++ Python/NextPermutation.py
@@ -0,0 +1,24 @@
+'''
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+The replacement must be in-place and use only constant extra memory.
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+'''
+def nextPermutation(self, nums): 
+
+    n, i = len(nums), len(nums) - 1
+    if n <=1:
+        return nums
+
+    while nums[i] <= nums[i-1] and i >0:
+        i -= 1
+    nums[i:] = reversed(nums[i:])
+
+    for j in range(i, n):
+        if nums[j] > nums[i-1]:
+            nums[i-1], nums[j] = nums[j], nums[i-1]                
+            return nums
