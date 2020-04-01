77011dd86a55d022a2df716d482d3a87e6413423
Hoda Zare <zare.hoda@gmail.com>
2020-04-01 00:02:14 -0400

    Product of Array Except Self

--- Python/ProductArrayExceptSelf.py
+++ Python/ProductArrayExceptSelf.py
@@ -1,4 +1,7 @@
-    
+    """
+    Given an array nums of n integers where n > 1,  
+    return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+    """
     def productExceptSelf(nums):
         
         output = [0] * len(nums)
